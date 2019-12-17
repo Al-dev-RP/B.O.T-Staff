63a33497926016136a3425011c39950b171105c5
Al-dev-RP <58971806+Al-dev-RP@users.noreply.github.com>
2019-12-17 09:01:32 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "!";
+
+client.login("NjU2MTcxMzI3MDE5MDI0Mzg0.Xfexag._q6aofPxJTchTTLIw0B0OPum6M8");
+
+client.on("guildMemberAdd", user =>{
+
+    user.guild.channel.get("656182662385369098").send("Bienvenue" + user + "sur ce serveur. Lit les règles et profite bien ^^")
+    user.addRole("https://discord.gg/jvUXB3")
+});
+
+client.on("message", message =>{
+    if (!message.guild) return
+    if (message.content === prefix + "hello")
+    message.channel.send("Bonjour, comment vas-tu" + message.author + "?")   
+})
